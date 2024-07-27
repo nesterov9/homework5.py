8a231c18982da3758513f8647d6825821ef42ce2
nesterov9 <a.nesterov3@vk.com>
2024-07-27 12:55:16 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,13 +1,13 @@
 immutable_var = 1, 3 ,"BMW",'AUDI'
 print(immutable_var)
 #в кортеже не изменяются значения элементов ,т.к только в списке можно производить изменения . В выводе он мне напишет ошибку
-immutable_var=['bmw','audi','mers']
-(immutable_var[0])
-immutable_var[0]='kia'
-print(immutable_var)
-(immutable_var[1])
-(immutable_var[1])='lada'
-print(immutable_var)
-(immutable_var[2])
-immutable_var[2]='chery'
-print(immutable_var)
+immutable_list=['bmw','audi','mers']
+(immutable_list[0])
+immutable_list[0]='kia'
+print(immutable_list)
+(immutable_list[1])
+(immutable_list[1])='lada'
+print(immutable_list)
+(immutable_list[2])
+immutable_list[2]='chery'
+print(immutable_list)
